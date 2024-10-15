ef74d098fdbe0bbe7700f1a1947236e6ab018241
ramya72002 <nsriramya7@gmail.com>
2024-10-15 14:54:19 +0530

    fixed

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,13 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
-import LogoIcon from "../../assets/svg/logo.svg";
 import useGlobalStyles from "../../styles/globalStyles";
 
 const Logo = () => {
   const globalStyles = useGlobalStyles();
   return (
     <View style={globalStyles.mainLogo}>
-      <LogoIcon />
+      <Image
+        source={require("../../../assets/appIcon/icon4.png")}
+        style={globalStyles.logoImage} // Adjust the style for the image
+      />
       <Text style={globalStyles.headingFour}>Smartup</Text>
     </View>
   );
