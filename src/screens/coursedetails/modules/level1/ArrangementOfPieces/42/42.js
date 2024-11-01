9c1bff9fc59d302411931ed0cf634a4fef9cfd00
TanujKumar41 <tanujkumar191001@gmail.com>
2024-11-01 21:54:53 +0530

    module data added

---
+++ src/screens/coursedetails/modules/level1/ArrangementOfPieces/42/42.js
@@ -0,0 +1,244 @@
+import React, { useContext, useState } from "react";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { WebView } from "react-native-webview";
+import useGlobalStyles from "../../../../../../styles/globalStyles";
+import Header from "../../../../../../components/header/Header";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import RatingIcon from "../../../../../../assets/svg/ratingIcon.svg";
+import AboutTab from "../../../../../abouttab/AboutTab";
+import CurriculumTab from "../../../../../curriculumtab/CurriculumTab";
+import CommonButton from "../../../../../../components/commonbutton/CommonButton";
+import ThemeContext from "../../../../../../components/Theme/ThemeContext";
+import ClassIcon from "../../../../../../components/classicon/ClassIcon";
+import TimeIcon from "../../../../../../components/timeicon/TimeIcon";
+
+const T11 = () => {
+  const route = useRoute();
+  const globalStyles = useGlobalStyles();
+  const navigation = useNavigation();
+  const theme = useContext(ThemeContext);
+
+  const tabs = [
+    { id: 1, tab: "About" },
+    { id: 2, tab: "Curriculum" },
+  ];
+
+  const [activeTab, setActiveTab] = useState("About");
+  const [showVideo, setShowVideo] = useState(false); // State to toggle video display
+
+  const handleTabs = (item) => {
+    setActiveTab(item);
+  };
+
+  const renderTabs = () => {
+    switch (activeTab) {
+      case "About":
+        return <AboutTab />;
+      case "Curriculum":
+        return <CurriculumTab />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <View style={[styles.mainContain, { backgroundColor: theme.background }]}>
+      <ScrollView style={globalStyles.colorBG}>
+        <View style={styles.mediaContainer}>
+          {!showVideo ? (
+            <TouchableOpacity onPress={() => setShowVideo(true)}>
+              <Image
+                source={require("../../../../../../assets/images/thumbnail.png")}
+                style={styles.thumbnail}
+              />
+            </TouchableOpacity>
+          ) : (
+           
+            <WebView
+              source={{ uri: "https://youtube.com/embed/mQU48pZ3maM" }}
+              style={{ width: "100%", height: 300 }}
+              allowsFullscreenVideo
+            />
+          )}
+        </View>
+        <View style={globalStyles.container}>
+          <View style={styles.absoluteIMG}>
+            <Header label="" backBTNCLR="white" />
+          </View>
+        </View>
+        <View
+          style={[
+            globalStyles.contents,
+            styles.parallaxContainer,
+            { backgroundColor: theme.background },
+          ]}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "100%",
+              paddingBottom: "3%",
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <RatingIcon />
+            </View>
+          </View>
+          <Text style={globalStyles.headingFour}>
+          3.2 Dark Side
+          </Text>
+          <View style={styles.classDetailsAndPrice}>
+            <View style={styles.classDetails}>
+              <ClassIcon />
+              <Text style={{ color: theme.color }}>21 Classes</Text>
+              <Text style={{ color: theme.color }}>|</Text>
+              <TimeIcon />
+              <Text style={{ color: theme.color }}>42 Hours</Text>
+            </View>
+          </View>
+          <View style={styles.tabContainer}>
+            {tabs.map((item) => (
+              <TouchableOpacity
+                key={item.id}
+                onPress={() => handleTabs(item.tab)}
+              >
+                <Text
+                  style={
+                    activeTab === item.tab
+                      ? styles.activeTab
+                      : styles.inactiveTab
+                  }
+                >
+                  {item.tab}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+          {/* {renderTabs()} */}
+          <Text style={globalStyles.paragraph}>
+          On the dark side, the arrangement mirrors that of the light side, with the board also positioned to have a white square on the right. Here, the back row features the same sequence of pieces, but the black queen starts on a black square, maintaining symmetry. The pawns stand ready to push forward, matching their opponent’s moves. This standardized setup ensures that both sides begin with identical formations, creating a balanced and fair environment for tactical maneuvers and strategic play as the game unfolds.
+
+          </Text>
+          
+        </View>
+      </ScrollView>
+      <View style={[styles.buttonContainer, { backgroundColor: theme.background }]}>
+        <TouchableOpacity 
+          style={styles.navigationButton}
+          onPress={() => navigation.goBack()} // Replace with your actual logic for Previous
+        >
+          <Text style={styles.buttonText}>Previous</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.navigationButton}
+          onPress={() => navigation.navigate("coursedetails/modules/level1/theChessboard/21")} // Replace with your next screen name
+        >
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: "50%",
+    height: 320,
+    marginTop: "8%",
+  },
+  thumbnail: {
+    width: "100%",
+    height: 300, // Adjust height as necessary
+    marginTop: "8%",
+  },
+  absoluteIMG: {
+    position: "absolute",
+    top: -270,
+    width: "100%",
+  },
+  parallaxContainer: {
+    marginTop: -120,
+    width: "100%",
+    alignItems: "flex-start",
+    paddingTop: "5%",
+    backgroundColor: "white",
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+  },
+  classDetailsAndPrice: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  classDetails: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: "3%",
+    gap: 10,
+  },
+  tabContainer: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: "5%",
+  },
+  activeTab: {
+    paddingHorizontal: "17%",
+    paddingVertical: "5%",
+    borderRadius: 35,
+    backgroundColor: "#FC4F72",
+    color: "white",
+    fontWeight: "600",
+  },
+  inactiveTab: {
+    paddingHorizontal: "17%",
+    paddingVertical: "5%",
+    borderRadius: 35,
+    backgroundColor: "#F1F1F1",
+    color: "black",
+    fontWeight: "600",
+  },
+ buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+    backgroundColor: "white", // Adjust if necessary
+  },
+  navigationButton: {
+    flex: 1,
+    marginHorizontal: 5,
+    padding: 15,
+    backgroundColor: "#FC4F72", // Change to your desired color
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  mainContain: {
+    flex: 1,
+  },
+  mediaContainer: {
+    marginVertical: 20,
+  },
+});
+
+export default T11;
---
+++ src/screens/coursedetails/modules/level1/specialMoves/51/51.js
@@ -0,0 +1,264 @@
+import React, { useContext, useState } from "react";
+import {
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { WebView } from "react-native-webview";
+import useGlobalStyles from "../../../../../../styles/globalStyles";
+import Header from "../../../../../../components/header/Header";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import RatingIcon from "../../../../../../assets/svg/ratingIcon.svg";
+import AboutTab from "../../../../../abouttab/AboutTab";
+import CurriculumTab from "../../../../../curriculumtab/CurriculumTab";
+import CommonButton from "../../../../../../components/commonbutton/CommonButton";
+import ThemeContext from "../../../../../../components/Theme/ThemeContext";
+import ClassIcon from "../../../../../../components/classicon/ClassIcon";
+import TimeIcon from "../../../../../../components/timeicon/TimeIcon";
+
+const T11 = () => {
+  const route = useRoute();
+  const globalStyles = useGlobalStyles();
+  const navigation = useNavigation();
+  const theme = useContext(ThemeContext);
+
+  const tabs = [
+    { id: 1, tab: "About" },
+    { id: 2, tab: "Curriculum" },
+  ];
+
+  const [activeTab, setActiveTab] = useState("About");
+  const [showVideo, setShowVideo] = useState(false); // State to toggle video display
+
+  const handleTabs = (item) => {
+    setActiveTab(item);
+  };
+
+  const renderTabs = () => {
+    switch (activeTab) {
+      case "About":
+        return <AboutTab />;
+      case "Curriculum":
+        return <CurriculumTab />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <View style={[styles.mainContain, { backgroundColor: theme.background }]}>
+      <ScrollView style={globalStyles.colorBG}>
+        <View style={styles.mediaContainer}>
+          {!showVideo ? (
+            <TouchableOpacity onPress={() => setShowVideo(true)}>
+              <Image
+                source={require("../../../../../../assets/images/thumbnail.png")}
+                style={styles.thumbnail}
+              />
+            </TouchableOpacity>
+          ) : (
+           
+            <WebView
+              source={{ uri: "https://youtube.com/embed/watch?v=TWgcG2MOLA8" }}
+              style={{ width: "100%", height: 300 }}
+              allowsFullscreenVideo
+            />
+          )}
+        </View>
+        <View style={globalStyles.container}>
+          <View style={styles.absoluteIMG}>
+            <Header label="" backBTNCLR="white" />
+          </View>
+        </View>
+        <View
+          style={[
+            globalStyles.contents,
+            styles.parallaxContainer,
+            { backgroundColor: theme.background },
+          ]}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              width: "100%",
+              paddingBottom: "3%",
+            }}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 5,
+              }}
+            >
+              <RatingIcon />
+            </View>
+          </View>
+          <Text style={globalStyles.headingFour}>
+          4.1 Castling
+          </Text>
+          <View style={styles.classDetailsAndPrice}>
+            <View style={styles.classDetails}>
+              <ClassIcon />
+              <Text style={{ color: theme.color }}>21 Classes</Text>
+              <Text style={{ color: theme.color }}>|</Text>
+              <TimeIcon />
+              <Text style={{ color: theme.color }}>42 Hours</Text>
+            </View>
+          </View>
+          <View style={styles.tabContainer}>
+            {tabs.map((item) => (
+              <TouchableOpacity
+                key={item.id}
+                onPress={() => handleTabs(item.tab)}
+              >
+                <Text
+                  style={
+                    activeTab === item.tab
+                      ? styles.activeTab
+                      : styles.inactiveTab
+                  }
+                >
+                  {item.tab}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+          {/* {renderTabs()} */}
+          <Text style={globalStyles.paragraph}>
+          Castling is a unique move in chess involving the king and a rook, primarily for defensive purposes. To castle, several conditions must be met: neither the king nor the rook can have moved previously, the squares between them must be unoccupied, and the king must not be in check, nor can it move through or land on a square under attack. During castling, the king moves two squares towards the rook, and the rook moves to the square immediately next to the king. This maneuver can be performed on either side of the board. Kingside castling involves the king moving towards the h-file rook, while queenside castling involves the king moving towards the a-file rook.
+
+          </Text>
+          <Text style={globalStyles.paragraph}>
+          <ol style={{ fontFamily: 'Montserrat, sans-serif', color: 'black', fontSize: '16px' }}>
+      <li>The King and Rook involved in the castling move must not have moved previously.</li>
+      <li>There must be no pieces between the Rook and King on the castling side.</li>
+      <li>Neither the King nor any squares on the castling side can be under check during the castling move.</li>
+    </ol>
+          </Text>
+          <Text style={globalStyles.paragraph}>
+          Kingside castling, also known as short castling, is a strategic chess move where the king moves two squares toward the rook on its starting side, and the rook then moves to the square right next to the king. This maneuver is primarily used to increase the king's safety by positioning it behind a protective wall of pawns and connecting the rooks, which enhances their ability to work together. For kingside castling to be legal, several conditions must be met: neither the king nor the rook involved in the castling move can have moved earlier in the game, there must be no pieces between the king and the rook, and the king cannot be in check or move through or land on a square that is under attack. Kingside castling is typically faster than queenside castling because it involves fewer squares, allowing the king to reach safety quickly and enabling the rook to take an active role in the game.
+
+          </Text>
+          <Text style={globalStyles.paragraph}>
+          Queenside castling, also known as long castling, is a strategic chess move where the king moves two squares toward the rook on the queen's side of the board, and the rook then moves to the square right next to the king. This move aims to enhance the king's safety while also bringing the rook into a more active and central position on the board. For queenside castling to be legal, several conditions must be met: the king and the rook involved in the castling must not have moved earlier in the game, there must be no pieces between the king and the rook, and the king cannot be in check, nor can it pass through or land on a square that is under attack.
+
+          </Text>
+          <Text style={globalStyles.paragraph}>
+          Compared to kingside castling, queenside castling often takes more time to set up because it involves more squares and usually requires more preparation, such as moving the queen and another piece out of the way. However, it can be a powerful move, as it often places the king in a more central position while allowing the rook to immediately control an important central file.
+
+          </Text>
+
+          
+        </View>
+      </ScrollView>
+      <View style={[styles.buttonContainer, { backgroundColor: theme.background }]}>
+        <TouchableOpacity 
+          style={styles.navigationButton}
+          onPress={() => navigation.goBack()} // Replace with your actual logic for Previous
+        >
+          <Text style={styles.buttonText}>Previous</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          style={styles.navigationButton}
+          onPress={() => navigation.navigate("coursedetails/modules/level1/theChessboard/21")} // Replace with your next screen name
+        >
+          <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    width: "50%",
+    height: 320,
+    marginTop: "8%",
+  },
+  thumbnail: {
+    width: "100%",
+    height: 300, // Adjust height as necessary
+    marginTop: "8%",
+  },
+  absoluteIMG: {
+    position: "absolute",
+    top: -270,
+    width: "100%",
+  },
+  parallaxContainer: {
+    marginTop: -120,
+    width: "100%",
+    alignItems: "flex-start",
+    paddingTop: "5%",
+    backgroundColor: "white",
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+  },
+  classDetailsAndPrice: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+  },
+  classDetails: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: "3%",
+    gap: 10,
+  },
+  tabContainer: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: "5%",
+  },
+  activeTab: {
+    paddingHorizontal: "17%",
+    paddingVertical: "5%",
+    borderRadius: 35,
+    backgroundColor: "#FC4F72",
+    color: "white",
+    fontWeight: "600",
+  },
+  inactiveTab: {
+    paddingHorizontal: "17%",
+    paddingVertical: "5%",
+    borderRadius: 35,
+    backgroundColor: "#F1F1F1",
+    color: "black",
+    fontWeight: "600",
+  },
+ buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+    backgroundColor: "white", // Adjust if necessary
+  },
+  navigationButton: {
+    flex: 1,
+    marginHorizontal: 5,
+    padding: 15,
+    backgroundColor: "#FC4F72", // Change to your desired color
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
+  mainContain: {
+    flex: 1,
+  },
+  mediaContainer: {
+    marginVertical: 20,
+  },
+});
+
+export default T11;
