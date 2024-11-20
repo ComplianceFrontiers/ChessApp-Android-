90e641365030d7534477b4a02cea06739028bd54
ramya72002 <nsriramya7@gmail.com>
2024-11-20 14:25:06 +0530

    formated

--- src/screens/message/MessageScreen.js
+++ src/screens/message/MessageScreen.js
@@ -11,25 +11,22 @@ const MessageScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, backgroundColor: "#fff" }}>
       <View 
-  style={{ 
-    position: "fixed", 
-    zIndex: 7, 
-    backgroundColor: "#fff", // White background color
-    marginBottom: "0%", 
-    paddingHorizontal: "10%", 
-    paddingVertical: "0%", 
-    paddingLeft: "10%", 
-    top: 60, 
-    left: 0 
-  }}
->
-  <HomeTab />
-</View>
+          style={{ 
+            position: "fixed", 
+            zIndex: 7, 
+            backgroundColor: "#fff", // White background color
+            marginBottom: "0%", 
+            paddingHorizontal: "10%", 
+            paddingVertical: "0%", 
+            paddingLeft: "10%", 
+            top: 60, 
+            left: 0 
+          }}
+        >
+          <HomeTab />
+    </View>
 
-
-      {/* Scrollable Content */}
       <ScrollView contentContainerStyle={styles.scrollContent}>
-        {/* Adding space below HomeTab */}
         <View style={{ marginTop: 80 }}>
         <Header label="Messages" backBTN={false} />
           <View contentContainerStyle={styles.contents}>
